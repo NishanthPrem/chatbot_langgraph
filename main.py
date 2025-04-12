2fa2e5ba343d8bfaee59dc87b9873264dc0bb908
Nishanth Prem <nishanthprem8@gmail.com>
2025-04-12 14:38:09 -0500

    Fix chatbot function to use llm_with_tools for message invocation

--- main.py
+++ main.py
@@ -14,19 +14,23 @@ from langgraph.prebuilt import ToolNode, tools_condition
 load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+
 # Creating the state State class capable of storing messages
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
+
 # The chatbot function that will be called by the graph
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 # Stream function to get the updates from the graph
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
         for value in event.values():
             print("Assistant:", value["messages"][-1].content)
+
 
 # Initializing the search tool and language model
 search = TavilySearchResults(max_results=2)
