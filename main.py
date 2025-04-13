ad8f1708f22510ffd3c6782fabadab2e293812b8
Nishanth Prem <nishanthprem8@gmail.com>
2025-04-13 10:06:15 -0500

    Add human assistance tool to chatbot and update tools initialization

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from typing import Annotated
 from typing_extensions import TypedDict
 
 from langchain_openai import ChatOpenAI
+from langchain.tools import tool
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.types import Command, Interrupt
 
 # Loading the environment variables from .env file
 load_dotenv()
@@ -19,6 +21,13 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 # Creating the state State class capable of storing messages
 class State(TypedDict):
     messages: Annotated[list, add_messages]
+
+
+@tool
+def human_assitance(query: str) -> str:
+    """Request assistance from the human"""
+    human_response = Interrupt({"query": query})
+    return human_response["data"]
 
 
 # The chatbot function that will be called by the graph
@@ -39,12 +48,12 @@ def stream_graph_updates(user_input: str):
 
 # Initializing the search tool and language model
 search = TavilySearchResults(max_results=2)
-tools = [search]
+tools = [search, human_assitance]
 llm = ChatOpenAI()
 llm_with_tools = llm.bind_tools(tools)
 
 
-tool_node = ToolNode(tools=tools)
+tool_node = ToolNode(tools)
 
 # Building the state graph
 graph_builder = StateGraph(State)
